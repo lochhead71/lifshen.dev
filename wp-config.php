c139e2a6c1bc5c1e75ca068ad52978bff5a7683a
Jim Lochhead <lochhead71@gmail.com>
2016-07-07 10:45:13 -0500

    Changed 'localhost' to '104.131.152.32' in config file.

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpress_user');
 define('DB_PASSWORD', 'marny');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '104.131.152.32');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
